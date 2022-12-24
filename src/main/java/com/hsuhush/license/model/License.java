fe821a5e3dfc47ef2086a8085380ae5e8b9f4b8d
akma517 <akma517@naver.com>
2022-12-24 21:55:26 +0900

    헬스체크 추가
    spring actuator를 사용한 마이크로 서비스 헬스체크 기능 추가
    application.properties로 헬스체크 기능 커스텀 가능

--- src/main/java/com/hsuhush/license/model/License.java
+++ src/main/java/com/hsuhush/license/model/License.java
@@ -3,8 +3,10 @@ package com.hsuhush.license.model;
 import org.springframework.hateoas.RepresentationModel;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class License extends RepresentationModel<License>{ // HATEOAS를 위한 확장 추가
     
     private int id;
