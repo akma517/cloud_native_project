76384992bc57faeaee829b6eeb7ce134b99be815
akma517 <akma517@naver.com>
2023-01-29 12:04:08 +0900

    도커 컴포즈를 통해 DB서버와 환경서버에 연동하여 구동될 수 있도록 보완
    진짜진짜 너무 복잡하고 힘드네요

---
+++ docker/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.organizations VALUES ('3', 'HusHush', 'Illary Huaylupo', 'hus1111@gmail.com', '777777777');
+INSERT INTO public.organizations VALUES ('2', 'Administrator', 'Admin', 'hushush2112@gmail.com', '888888888');
+INSERT INTO public.organizations VALUES ('1', 'HusHush', 'Illary Huaylupo', 'hus2111@gmail.com', '666666666');
+INSERT INTO public.licenses VALUES ('2', '2', 'Software Product', 'HusHush', 'complete', 'I AM DEV');
+INSERT INTO public.licenses VALUES ('1', '1', 'Software Product', 'HusHush', 'complete', 'I AM DEV');
